afb5b9c0996bdf5ee72b72057a8ced5766ebf469
deeate8 <deejordan0820@gmail.com>
2025-03-24 16:14:16 +0900

    エラー

---
+++ MatchEigo/Question.swift
@@ -0,0 +1,17 @@
+//
+//  Question.swift
+//  MatchEigo
+//
+//  Created by Dee Jordan on 22/3/2025.
+//
+
+//import Foundation
+//import RealmSwift
+//
+//class Question {
+//    
+//    var questionTitle = ""
+//    var isMatched = false
+//    
+//    
+//}
