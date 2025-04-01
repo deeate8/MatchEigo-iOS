e386f9fb74a1055fb0beefd8d3fdfd85c493019b
deeate8 <deejordan0820@gmail.com>
2025-04-02 00:01:41 +0900

    ロジック作成1

--- MatchEigo/EmojiGameViewController.swift
+++ MatchEigo/EmojiGameViewController.swift
@@ -16,7 +16,7 @@ class EmojiGameViewController: UIViewController {
     
     private let realm = try! Realm()
     private var currentUser: UserScore?
-
+    
     
     
     let wordPairs = [
@@ -36,7 +36,8 @@ class EmojiGameViewController: UIViewController {
         setupEmojiGame()
     }
     
-    func setupEmojiGame() { //clear prev selections
+    func setupEmojiGame() {
+        //clear prev selections
         selectedQuestionButton = nil
         score = 0
         
@@ -72,8 +73,55 @@ class EmojiGameViewController: UIViewController {
         
         let questionWord = wordPairs[questionIndex].0
         let answerWord = wordPairs[answerIndex].1
-
+        
+        
+        // Checking pairs
+        if wordPairs.contains(where: { $0 == (questionWord, answerWord) }) {
+            score += 1
+            questionButton.isEnabled = false
+            sender.isEnabled = false
+            UIView.animate(withDuration: 0.3, animations: {
+                questionButton.backgroundColor = .systemGreen
+                sender.backgroundColor = .systemGreen
+            })
+        } else {
+            UIView.animate(withDuration: 0.3, animations: {
+                questionButton.backgroundColor = .systemRed
+                sender.backgroundColor = .systemRed
+            }, completion: { _ in
+                UIView.animate(withDuration: 0.3) {
+                questionButton.backgroundColor = .systemBlue
+                sender.backgroundColor = .systemBlue
+            }
+        })
     }
-    
-    
+        
+        selectedQuestionButton = nil
 }
+    
+    
+    @IBAction func backTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Leave Game?", message: "Your progress will be saved :)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Stay", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Leave", style: .default) { _ in
+            let transition = CATransition()
+            
+//            3D Block animation
+            transition.type = CATransitionType(rawValue: "cube")
+                    transition.subtype = .fromLeft
+                    transition.duration = 0.8
+                    transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+                    self.view.window?.layer.add(transition, forKey: nil)
+                    self.tabBarController?.selectedIndex = 0
+            
+                    if let window = self.view.window {
+                        window.layer.add(transition, forKey: kCATransition)
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let tabBarController = storyboard.instantiateViewController(withIdentifier: "MainTabBar") as! UITabBarController
+                        window.rootViewController = tabBarController
+                }
+        })
+        present(alert, animated: true)
+    }
+    // TODO: saving data in Realm
+            }
---
+++ MatchEigo/WordPair.swift
@@ -0,0 +1,14 @@
+//
+//  WordPair.swift
+//  MatchEigo
+//
+//  Created by Dee Jordan on 28/3/2025.
+//
+
+import Foundation
+
+struct WordPair {
+    let question: String
+    let answer: String
+    let isMatch: Bool
+}
